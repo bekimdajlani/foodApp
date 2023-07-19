4ebb47e8ef96d0635ef1d27d5adf1318762935e0
BekimDajlani <bekim_dajlani@yahoo.com>
2023-07-19 16:19:56 +0200

    Added Modal Component

--- src/components/Cart/Modal.js
+++ src/components/Cart/Modal.js
@@ -1,12 +1,31 @@
 import React from "react";
+import ReactDOM  from "react-dom";
 
 import classes from './modal.module.css'
 
+const Backdrop = (props) => {
+    return (
+        <div className={classes.backdrop}></div>
+    )
+};
 
-const Modal = () => {
+const ModalOverlay = (props) => {
+    return (
+        <div className={classes.modal}>
+            <div className={classes.content}>{props.children}</div>
+        </div>
+    )
+};
+
+const portalElement = document.getElementById('modal-overlay');
+
+const Modal = (props) => {
 
     return(
-        <></>
+        <>
+            {ReactDOM.createPortal(<Backdrop/>, portalElement )}
+            {ReactDOM.createPortal(<ModalOverlay>{props.children}</ModalOverlay>,portalElement)}
+        </>
     )
 };
 
