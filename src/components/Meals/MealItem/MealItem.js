fb6a451d50b2e70fc94f600bbd5632f5a9a44259
BekimDajlani <bekim_dajlani@yahoo.com>
2023-07-22 13:16:33 +0200

    add item feature added

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,11 +1,25 @@
 import React from "react";
 
+import { useContext } from "react";
 import classes from './mealitem.module.css'
 import MealItemForm from "./MealItemForm";
+import CartContext from '../../../store/cart-context'
 
 const MealItem = (props) => {
 
+   const cartCtx = useContext(CartContext);
     const price = `$ ${props.price.toFixed(2)}`
+
+    const onAddToCartHandler = (amount) => {
+
+        cartCtx.addItem({
+            id: props.id,
+            name: props.name,
+            amount: amount,
+            price: props.price
+        });
+
+    };
 
     return (
         <li className={classes.meal}>
@@ -15,7 +29,7 @@ const MealItem = (props) => {
                 <div className={classes.price}>{price}</div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm onAddToCart={onAddToCartHandler}/>
             </div>
         </li>
     )
--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -2,16 +2,16 @@ import React from "react";
 
 import classes from './input.module.css';
 
-const Input = (props) => {
+const Input = React.forwardRef((props, ref) => {
 
 
     return (
         <div className={classes.input}>
             <label htmlFor={props.input.id}>{props.label}</label>
-            <input {... props.input}/> 
+            <input ref={ref} {... props.input}/> 
             {/* merr gjith vlerat qe do i kalohen si props dhe i shperndan vet */}
         </div>
     )
-};
+});
 
 export default Input;
